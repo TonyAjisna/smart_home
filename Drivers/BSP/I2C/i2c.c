a172c78b068cd4fa800f60926fea18fa855e4f5e
Tony <zhaobw@yeah.net>
2024-08-12 23:19:50 +0800

    添加部分IIC驱动

--- Drivers/BSP/I2C/i2c.c
+++ Drivers/BSP/I2C/i2c.c
@@ -1,1 +1,15 @@
 #include "i2c.h"
+
+I2C_HandleTypeDef  hi2c1;
+
+
+/* 
+ 1、先使能IIC的SCL和SDA对应的GPIO时钟 
+ 2、设置对应DPIO工作模式（开漏输出）
+ 3、参考IIC总线协议，编写信号函数（起始信号、停止信号、应答信号）
+ 4、编写IIC的读写函数
+*/
+void MX_I2C1_Init(void)
+{
+    // hi2c1.Instance = I2C1;
+}
--- Drivers/BSP/I2C/i2c.h
+++ Drivers/BSP/I2C/i2c.h
@@ -1,4 +1,50 @@
 #ifndef _I2C_H_
 #define _I2C_H_
 
+#include "main.h"
+#include "stm32f1xx_hal_i2c.h"
+#include "./SYSTEM/sys/sys.h"
+
+/* 引脚定义 */
+#define IIC_SCL_GPIO_PORT GPIOB
+#define IIC_SCL_GPIO_PIN GPIO_PIN_8
+#define IIC_SCL_GPIO_CLK_ENABLE()     \
+    do                                \
+    {                                 \
+        __HAL_RCC_GPIOB_CLK_ENABLE(); \
+    } while (0) /* PB口时钟使能 */
+
+#define IIC_SDA_GPIO_PORT GPIOB
+#define IIC_SDA_GPIO_PIN GPIO_PIN_9
+#define IIC_SDA_GPIO_CLK_ENABLE()     \
+    do                                \
+    {                                 \
+        __HAL_RCC_GPIOB_CLK_ENABLE(); \
+    } while (0) /* PB口时钟使能 */
+
+/* IO操作 */
+#define IIC_SCL(x)                                                                                                                                         \
+    do                                                                                                                                                     \
+    {                                                                                                                                                      \
+        x ? HAL_GPIO_WritePin(IIC_SCL_GPIO_PORT, IIC_SCL_GPIO_PIN, GPIO_PIN_SET) : HAL_GPIO_WritePin(IIC_SCL_GPIO_PORT, IIC_SCL_GPIO_PIN, GPIO_PIN_RESET); \
+    } while (0); /* SCL */
+
+#define IIC_SDA(x)                                                                                                                                         \
+    do                                                                                                                                                     \
+    {                                                                                                                                                      \
+        x ? HAL_GPIO_WritePin(IIC_SCL_GPIO_PORT, IIC_SCL_GPIO_PIN, GPIO_PIN_SET) : HAL_GPIO_WritePin(IIC_SCL_GPIO_PORT, IIC_SCL_GPIO_PIN, GPIO_PIN_RESET); \
+    } while (0); /* SDA */
+
+#define IIC_READ_SDA HAL_GPIO_ReadPin(IIC_SDA_GPIO_PORT, IIC_SDA_GPIO_PIN) /* 读取SDA */
+
+/* IIC所有操作函数 */
+void MX_I2C1_Init(void);
+void IIC_Start(void);                       /* 发送IIC开始信号 */
+void IIC_Stop(void);                        /* 发送IIC停止信号 */
+void IIC_Ack(void);                         /* IIC发送ACK信号 */
+void IIC_Nack(void);                        /* IIC不发送ACK信号 */
+uint8_t IIC_Wait_Ack(void);                 /* IIC等待ACK信号 */
+void IIC_Send_Byte(uint8_t txd);            /* IIC发送一个字节 */      
+uint8_t IIC_Read_Byte(unsigned char ack);   /* IIC读取一个字节 */
+
 #endif
